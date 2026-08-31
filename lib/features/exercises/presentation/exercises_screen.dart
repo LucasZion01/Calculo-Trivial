628a88bd068e05b14a78e6b61bfa7b7b2538c21a
LucasZion01 <128838361+LucasZion01@users.noreply.github.com>
2026-08-31 01:31:06 -0300

    feat(i18n): localize algebra exercise content

--- lib/features/exercises/presentation/exercises_screen.dart
+++ lib/features/exercises/presentation/exercises_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:calcquest/l10n/app_localizations.dart';
+import 'package:calcquest/shared/data/localized_algebra_exercise_content.dart';
 import 'package:calcquest/shared/data/mock_exercise_data.dart';
 import 'package:calcquest/shared/domain/exercise_review_item.dart';
 import 'package:calcquest/shared/state/app_progress.dart';
@@ -63,7 +64,10 @@ class _ExercisesScreenState extends State<ExercisesScreen> {
         .toList();
   }
 
-  ExerciseData get currentExercise => sessionExercises[currentExerciseIndex];
+  ExerciseData get currentExercise => localizeAlgebraExerciseContent(
+        sessionExercises[currentExerciseIndex],
+        Localizations.localeOf(context),
+      );
 
   bool get isLastExercise =>
       currentExerciseIndex == sessionExercises.length - 1;
