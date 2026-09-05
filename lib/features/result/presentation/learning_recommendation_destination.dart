264d0b1959d10b8dcc94abde88365a3bd751a523
LucasZion01 <128838361+LucasZion01@users.noreply.github.com>
2026-09-05 11:59:41 -0300

    fix: keep fallback callback for unknown modules

--- lib/features/result/presentation/learning_recommendation_destination.dart
+++ lib/features/result/presentation/learning_recommendation_destination.dart
@@ -2,7 +2,6 @@ import 'package:flutter/widgets.dart';
 
 import 'package:calcquest/shared/state/app_progress.dart';
 
-import '../../learning_path/presentation/learning_path_screen.dart';
 import '../../lesson/presentation/algebra_course_screen.dart';
 import '../../lesson/presentation/continuity_course_screen.dart';
 import '../../lesson/presentation/derivatives_course_screen.dart';
@@ -10,7 +9,7 @@ import '../../lesson/presentation/equations_course_screen.dart';
 import '../../lesson/presentation/functions_lesson_screen.dart';
 import '../../lesson/presentation/limits_course_screen.dart';
 
-Widget learningRecommendationDestinationFor(String moduleId) {
+Widget? learningRecommendationDestinationFor(String moduleId) {
   return switch (moduleId) {
     AppProgress.algebraFundamentalId => const AlgebraCourseScreen(),
     AppProgress.equationsAndInequationsId => const EquationsCourseScreen(),
@@ -18,6 +17,6 @@ Widget learningRecommendationDestinationFor(String moduleId) {
     AppProgress.limitsId => const LimitsCourseScreen(),
     AppProgress.continuityId => const ContinuityCourseScreen(),
     AppProgress.derivativesId => const DerivativesCourseScreen(),
-    _ => const LearningPathScreen(),
+    _ => null,
   };
 }
