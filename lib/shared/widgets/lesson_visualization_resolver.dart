d50aba08da6a4f903ec225683a420f095f220fd7
LucasZion01 <128838361+LucasZion01@users.noreply.github.com>
2026-09-05 01:48:14 -0300

    feat: integra novas visualizações às aulas

--- lib/shared/widgets/lesson_visualization_resolver.dart
+++ lib/shared/widgets/lesson_visualization_resolver.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 
 import 'package:calcquest/shared/domain/course_lesson_data.dart';
+import 'package:calcquest/shared/widgets/advanced_calculus_visualizations.dart';
 import 'package:calcquest/shared/widgets/calculus_visualizations.dart';
 
 Widget? lessonVisualizationFor(
@@ -11,10 +12,22 @@ Widget? lessonVisualizationFor(
     'limites-01-intuicao' => LimitApproachVisualization(
         isEnglish: isEnglish,
       ),
+    'limites-02-laterais' => OneSidedLimitVisualization(
+        isEnglish: isEnglish,
+      ),
+    'limites-06-infinito' => AsymptoteVisualization(
+        isEnglish: isEnglish,
+      ),
     'continuidade-01-significado' => ContinuityVisualization(
         isEnglish: isEnglish,
       ),
+    'continuidade-03-descontinuidades' => DiscontinuityTypesVisualization(
+        isEnglish: isEnglish,
+      ),
     'derivadas-01-significado' => DerivativeTangentVisualization(
+        isEnglish: isEnglish,
+      ),
+    'derivadas-06-tangente' => SecantToTangentVisualization(
         isEnglish: isEnglish,
       ),
     _ => null,
